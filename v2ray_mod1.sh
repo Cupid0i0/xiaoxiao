6343dd237c792b7c201d143923127cff711ffe24
Cupid0i0 <92776681+Cupid0i0@users.noreply.github.com>
2022-10-10 15:30:19 +0800

    Update v2ray_mod1.sh

--- v2ray_mod1.sh
+++ v2ray_mod1.sh
@@ -397,11 +397,11 @@ getData() {
         echo "   1) 静态网站(位于/usr/share/nginx/html)"
         echo "   2) 小说站(随机选择)"
         echo "   3) 美女站(https://imeizi.me)"
-        echo "   4) 高清壁纸站(https://bing.imeizi.me)"
+        echo "   4) 高清壁纸站(https://wallhaven.cc/)"
         echo "   5) 自定义反代站点(需以http或者https开头)"
         read -p "  请选择伪装网站类型[默认:高清壁纸站]" answer
         if [[ -z "$answer" ]]; then
-            PROXY_URL="https://bz.zzzmh.cn/index"
+            PROXY_URL="https://wallhaven.cc/"
         else
             case $answer in
             1)
@@ -427,7 +427,7 @@ getData() {
                 PROXY_URL="https://imeizi.me"
                 ;;
             4)
-                PROXY_URL="https://bz.zzzmh.cn/index"
+                PROXY_URL="https://wallhaven.cc/"
                 ;;
             5)
                 read -p " 请输入反代站点(以http或者https开头)：" PROXY_URL
